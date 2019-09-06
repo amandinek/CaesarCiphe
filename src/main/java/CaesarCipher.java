7e4e659714777fa7eca238201ad147b5be70b141
amandinek <karambiziga@gmail.com>
2019-09-06 12:27:48 +0200

    add condition

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,2 +1,24 @@
 public class CaesarCipher {
+    public static String encrypt(String plainText,int n){
+        if(n>26){
+            n=n%26;
+        }else if(n<26){
+            n=(n%26)+26;
+        }
+        String encryptText="";
+        int length= plainText.length();
+        for (int i=0;i<length;i++){
+            char ch = plainText.charAt(i);
+            if(Character.isLetter(ch)){
+
+            } else{
+                encrypt()
+            }
+        }
+        return encryptText;
+    };
+
+    public static void main(String[]args){
+
+    }
 }
