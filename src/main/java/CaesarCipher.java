d3a2c8d8f8680e89511e53757d1b3ca0faa21fb6
amandinek <karambiziga@gmail.com>
2019-09-06 14:35:31 +0200

    add decrypting conditions

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class CaesarCipher {
     public static String encrypt(String plainText,int n){
         if(n>26){
@@ -8,17 +9,79 @@ public class CaesarCipher {
         String encryptText="";
         int length= plainText.length();
         for (int i=0;i<length;i++){
-            char alpfa = plainText.charAt(i);
-            if(Character.isLetter(alpfa)){
+            char alpha = plainText.charAt(i);
+            if(Character.isLetter(alpha)){
+                if(Character.isLowerCase(alpha)){
+                    char al = (char)(alpha+n);
+                    if(al>'z'){
+                        encryptText += (char) (n - (26 + n));
+                    } else{
+                        encryptText +=al;
+                    }
+                }
+                } else if(Character.isUpperCase(alpha)){
+                    char al = (char)(alpha+n);
+                    if(al>'Z'){
+                        encryptText += (char) (n - (26 + n));
+                    } else{
+                        encryptText +=al;
+                    }
 
-            } else{
-                encryptText += alpfa;
+                }else{
+                encryptText += alpha;
+
+            }
+            }
+        return encryptText;
+        }
+    public static String dencrypt(String plainText,int n){
+        if(n>26){
+            n=n%26;
+        }else if(n<26){
+            n=(n%26)+26;
+        }
+        String encryptText="";
+        int length= plainText.length();
+        for (int i=0;i<length;i++){
+            char alpha = plainText.charAt(i);
+            if(Character.isLetter(alpha)){
+                if(Character.isLowerCase(alpha)){
+                    char al = (char)(alpha+n);
+                    if(al<'a'){
+                        encryptText += (char) (n + (26 - n));
+                    } else{
+                        encryptText +=al;
+                    }
+                }
+            } else if(Character.isUpperCase(alpha)){
+                char al = (char)(alpha+n);
+                if(al<'A'){
+                    encryptText += (char) (n + (26 -n));
+                } else{
+                    encryptText +=al;
+                }
+
+            }else{
+                encryptText += alpha;
+
             }
         }
         return encryptText;
-    };
+    }
+
+
 
     public static void main(String[]args){
+        System.out.println("welcome to encript & decrypt");
+        System.out.println("welcome to encript & decrypt");
+        Scanner cipher= new Scanner(System.in);
+        String message=cipher.nextLine();
+        String encryptText=encrypt(message,3);
+        String decryptText=dencrypt(message, 3);
+        System.out.println(encryptText);
+        System.out.println(decryptText);
+
+
 
     }
 }
